80e11e580702d2a685a50eb1151e27dbddd9219b
David Low <musumashae@gmail.com>
2017-06-08 16:27:02 -0400

    added functionality to submit button

--- front_end_dev_class_project/js/app.js
+++ front_end_dev_class_project/js/app.js
@@ -204,7 +204,7 @@ var menu = {
 
 
 let table = document.getElementById('foodMenu');
-let menuHTML;
+let menuHTML = ``;
 
 //Function adds menu categories from the section argument ("Food" or "Beverages") then ivokes the itemAdd function to add the specific items of that category
 function menuCategoryAdd(section) {
@@ -219,11 +219,11 @@ section.forEach(function(category) {
                 </tr>
             </thead>
             <tbody>`;
-  itemAdd(category);
+  menuItemAdd(category);
 });
 }
 //Function takes the category as an argument and retrieves and adds the specific items to that category
-function itemAdd(category) {
+function menuItemAdd(category) {
     category.items.forEach(function(item) {
         menuHTML +=
             `<tr>
@@ -273,21 +273,30 @@ function isValidEmailAddress(emailAddress) {
     var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
     return pattern.test(emailAddress);
 };
-var valid = $('<p></p>').insertAfter('.input');
-
+var $valid = $('<p></p>').insertAfter('.input');
+var $button = $('.button');
+//Sets default state of button to 'disabled'
+$button.prop('disabled', true);
+//adds event listener to the email input field
 $('.input').keyup(function() {
   //Value of email input
-  var value = $(this).val();
+  var $value = $(this).val();
   //Tests if email is valid
-  if(isValidEmailAddress(value) === true) {
+  if(isValidEmailAddress($value) === true) {
   //Displays message stating email is valid and adds appropriate css class
-    $(valid).text("Email Address is Valid!").removeClass('inValid').addClass('valid');
+    $valid.text("Email Address is Valid!").removeClass('inValid').addClass('valid');
+    $button.prop('disabled', false);
   } else {
   //Displays message stating email is invalid and adds appropriate css class
-    $(valid).text("Email Address is NOT Valid!").addClass('inValid');
+    $valid.text("Email Address is NOT Valid!").addClass('inValid');
+    $button.prop('disabled', true);
   }
 });
 
-
+//Prevents default behaviour of submit button and triggers an alert box when clicked
+$button.click(function() {
+  event.preventDefault();
+  alert("Server unavailable, try again later!");
+});
 
 
